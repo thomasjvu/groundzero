ae32044802264dd4ef5cf3f048c6aaddde8fb563
Thomas Vu <thomasjvu@gmail.com>
2023-05-14 21:52:29 -0400

    add category types

---
+++ src/types/category.ts
@@ -0,0 +1,15 @@
+export interface Category {
+    name: string;
+    value: string;
+}
+
+export const categoryList: Category[] = [
+    { name: "Art", value: 'art' },
+    { name: "Communications", value: 'communications' },
+    { name: "Content Creation", value: 'content-creation' },
+    { name: "Education", value: 'education' },
+    { name: "Game Design", value: 'game-design' },
+    { name: "Game Development", value: 'game-development' },
+    { name: "Production", value: 'production' },
+    { name: "Software Development", value: 'software-development' },
+]
