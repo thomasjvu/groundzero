0518b584a9c333329c5cee0fb648e1410ed552ba
Thomas Vu <thomasjvu@gmail.com>
2023-05-15 15:29:16 -0400

    Update error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/Home";
+import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Profile from './pages/Profile';
 import Listing from './pages/Listing';
-import Post from './pages/Post'
+import Post from './pages/Post';
 import Jobs from './pages/Jobs';
-import NotFound from './pages/NotFound';
 
 import Terms from './pages/Legal/Terms';
 import Privacy from './pages/Legal/Privacy';
 import Disclaimers from './pages/Legal/Disclaimers';
 
-// import ListingDetails from './components/ListingDetails';
+import NotFound from './pages/Error/NotFound';
+import ErrorBoundary from './components/ErrorBoundary';
 
-const App: React.FC = () => {
+const App: React.FC = (): JSX.Element => {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/post" element={<Post />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/jobs" element={<Jobs />} />
-                <Route path="/jobs/:id" element={<Listing />} />
-                <Route path="*" element={<NotFound />} />
-                <Route path="/privacy" element={<Privacy />} />
-                <Route path="/disclaimers" element={<Disclaimers />} />
-                <Route path="/terms" element={<Terms />} />
-            </Routes>
-        </Router>
+        <ErrorBoundary>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/post" element={<Post />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/jobs" element={<Jobs />} />
+                    <Route path="/jobs/:id" element={<Listing />} />
+                    <Route path="/privacy" element={<Privacy />} />
+                    <Route path="/disclaimers" element={<Disclaimers />} />
+                    <Route path="/terms" element={<Terms />} />
+
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+        </ErrorBoundary>
     );
-}
+};
 
 export default App;
