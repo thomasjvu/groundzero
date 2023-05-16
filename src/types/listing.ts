96ee23d41348470ea60a3938d09b5ba51bc54f0d
Thomas J. Vu <thomasjvu@gmail.com>
2023-05-16 15:48:26 -0400

    cleanup code

--- src/types/listing.ts
+++ src/types/listing.ts
@@ -1,20 +1,20 @@
-// import { Category } from "./category";
-// import { Rate } from "./rate";
-// import { Setting } from "./setting";
-// import { Level } from "./level";
-// import { Contract } from "./contract";
-// import { Location } from "./location";
+import { Category } from "./category";
+import { Rate } from "./rate";
+import { Setting } from "./setting";
+import { Level } from "./level";
+import { Contract } from "./contract";
+import { Location } from "./location";
 
 export interface Listing {
     id: string;
     title: string;
-    category?: string;
+    category?: Category;
     created_at: string;
-    level?: string;
-    contract?: string;
-    location?: string;
-    setting?: string;
-    rate?: string;
+    level?: Level;
+    contract?: Contract;
+    location?: Location;
+    setting?: Setting;
+    rate?: Rate;
     min_wage?: number;
     max_wage?: number;
     company_id?: string;
